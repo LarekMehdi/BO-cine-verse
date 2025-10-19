8a4dc3d88ea7dcb1cbca12d253e731da40a50c47
mehdi-larek <larek.med@gmail.com>
2025-10-19 13:19:38 +0200

    init spring

---
+++ src/main/java/fr/perso/cineverse/CineverseApplication.java
@@ -0,0 +1,13 @@
+package fr.perso.cineverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineverseApplication.class, args);
+	}
+
+}
