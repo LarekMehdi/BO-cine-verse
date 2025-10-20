812c249e4c6b343d4b91b0507488a9910c6205e0
mehdi-larek <larek.med@gmail.com>
2025-10-20 15:58:37 +0200

    oublie du path signup

--- src/main/java/fr/perso/cineverse/auth/AuthController.java
+++ src/main/java/fr/perso/cineverse/auth/AuthController.java
@@ -1,6 +1,7 @@
 package fr.perso.cineverse.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class AuthController {
 
      /** SIGNUP **/
 
+     @PostMapping("signup")
     public User signup(@RequestBody @Valid UserDto dto) {
         return this.authService.signup(dto);
     }
